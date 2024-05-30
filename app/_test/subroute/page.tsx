08c137db45364e8f99f09449599566f65c5c2e15
saswat <saswatsingh95747@gmail.com>
2024-05-30 23:20:03 +0530

    inititalized project

---
+++ app/_test/subroute/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        i m never 
+    </div>
+  )
+}
+
+export default page
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://alert-bulldog-27.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
